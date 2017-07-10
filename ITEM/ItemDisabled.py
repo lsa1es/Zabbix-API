d3d7009d9c16dbd4570219240ee4ac2bb1a3c2ef
Luiz Sales <lsa1es@users.noreply.github.com>
2017-07-10 17:50:55 -0300

    Create ItemDisabled.py

---
+++ ITEM/ItemDisabled.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Luiz Sales
+# luiz@lsales.biz
+
+# Desabilitar item baseado na chave que ele recebe
+
+from zabbix_api import *
+import sys
+
+HOST = sys.argv[1]
+CHAVE = sys.argv[2]
+
+zapi = ZabbixAPI(server="")
+zapi.login("", "" )
+
+host_get = zapi.host.get({ "output": "hostid", "filter": { "name" : HOST } })
+for x in host_get:
+	hId = x[u'hostid']
+	item_get = zapi.item.get({ "output": "extend", "hostids" : hId, "search" : { "key_" : CHAVE } })
+	for w in item_get:
+		iId = w[u'itemid']
+		item_update = zapi.item.update({ "itemid" : iId, "status": 1 }) 
+		
+    
+   
